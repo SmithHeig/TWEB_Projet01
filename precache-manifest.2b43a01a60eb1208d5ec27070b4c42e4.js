3325de0a0b6d90db055f8b38ca5fb4f72deb4a89
James <james.smith@heig-vd.ch>
2018-10-28 13:08:35 +0100

    Updates

---
+++ precache-manifest.2b43a01a60eb1208d5ec27070b4c42e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ab0a8b91d43523d5d79",
+    "url": "/static/js/main.7ab0a8b9.chunk.js"
+  },
+  {
+    "revision": "ca54633ca766d21b6ec3",
+    "url": "/static/js/1.ca54633c.chunk.js"
+  },
+  {
+    "revision": "7ab0a8b91d43523d5d79",
+    "url": "/static/css/main.40b2c742.chunk.css"
+  },
+  {
+    "revision": "020b8fd01af9e8d65bd5af2e7c908949",
+    "url": "/index.html"
+  }
+];
